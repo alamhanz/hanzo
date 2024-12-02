0a197c940a3e6ab3015b3228d6aaee791eba7c4d
alamhanz <alamhanz@gmail.com>
2024-12-02 12:28:28 +0900

    testing

--- test.py
+++ test.py
@@ -6,13 +6,11 @@ load_dotenv()
 # vdb = vectordb(model="BAAI/bge-large-en-v1.5")
 vdb = vectordb(
     model="BAAI/bge-large-en-v1.5",
-    file="../datakoen/app/default/my_profile.txt",
-    db_path="../../datakoen/app/default/about_me/",
+    file="test.txt",
+    db_path="default/test/",
 )
 vdb.create(chunk_size=100, chunk_overlap=20)
 # vdb.load()
-
-# print(vdb.db.similarity_search_with_relevance_scores("who is alamsyah", k=15))
 
 # hanzo_talk = talk(vdb.db, model="meta-llama/Meta-Llama-3-8B-Instruct-Lite")
 ## Not all model in together enable to return json (https://docs.together.ai/docs/json-mode)
